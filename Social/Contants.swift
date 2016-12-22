3ac4936b40cb8a9948e04459b00be6a855334ec1
Yujin K <seongki1@me.com>
2016-12-22 19:05:48 +0900

    Sign in ui costom classes complete

---
+++ Social/Contants.swift
@@ -0,0 +1,11 @@
+//
+//  Contants.swift
+//  Social
+//
+//  Created by Kiwon on 2016. 12. 22..
+//  Copyright © 2016년 mgrdoc. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY : CGFloat = 120.0 / 255.0
---
+++ Social/FancyBtn.swift
@@ -0,0 +1,27 @@
+//
+//  FancyBtn.swift
+//  Social
+//
+//  Created by Kiwon on 2016. 12. 22..
+//  Copyright © 2016년 mgrdoc. All rights reserved.
+//
+
+import UIKit
+
+class FancyBtn: UIButton {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize (width: 1.0, height: 1.0)
+        
+        layer.cornerRadius = 2.0
+        
+        
+    }
+
+    
+}
