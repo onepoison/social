3f293ccca030be15462b0c4c2e646fd41ed41bb4
Yujin K <seongki1@me.com>
2017-01-01 02:34:42 +0900

    feed-table complete

---
+++ Social/PostCell.swift
@@ -0,0 +1,25 @@
+ //
+//  PostCell.swift
+//  Social
+//
+//  Created by Kiwon on 2017. 1. 1..
+//  Copyright © 2017년 mgrdoc. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    @IBOutlet weak var profileImg : UIImageView!
+    @IBOutlet weak var userNameLbl : UILabel!
+    @IBOutlet weak var postImg : UIImageView!
+    @IBOutlet weak var caption : UITextView!
+    @IBOutlet weak var likesLbl : UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+
+
+}
